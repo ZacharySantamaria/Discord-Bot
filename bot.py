2be4dcf12c0c67f44969e3ecf60b75297f178035
Zachary Santamaria <zsantamaria@protonmail.com>
2022-04-25 23:17:14 -0400

    Fixing the format for the super-linter to pass

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
-import discord
-from discord.ext import commands
+"""The main of the bot"""
+
 from os import getenv
+from discord.ext import commands
 from dotenv import load_dotenv
+import discord
+
 
 bot = commands.Bot(command_prefix=['~', 'bot '])
 startup_extensions = ['functionality']
@@ -9,13 +12,14 @@ startup_extensions = ['functionality']
 
 @bot.event
 async def on_ready():
-  
-  await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="with your mother"))
-  
-  for extension in startup_extensions:
-    bot.load_extension(extension)
+    """Once the bot is connected this startup function for the bot"""
+    await bot.change_presence(
+      activity=discord.Activity(type=discord.ActivityType.playing, name="with your mother"))
 
-  print('Bot is connected')
+    for extension in startup_extensions:
+        bot.load_extension(extension)
+
+    print('Bot is connected')
 
 
 load_dotenv()
--- functionality.py
+++ functionality.py
@@ -1,81 +1,80 @@
+"""Creation of a class to add functionality to the bot. These will be general things like 
+text listners and small commands."""
 from discord.ext import commands
 from requests import post
 # import asyncio
 
-class functionality(commands.Cog):
+ZACK_ID = 288899751850672138
+YAS_ID = 474609160487698463
+OMARI_ID = 443940248993398786
+SANITA_ID = 438175368483176448
+XAVI_ID = 186427201103593472
+DONAL_ID = 357336111158263810
 
-  def __init__(self, bot):
-    self.bot = bot
+class Functionality(commands.Cog):
+    """A class that holds the functionality of the bot"""
+    def __init__(self, bot):
+        self.bot = bot
+        self.yas_count = 0
+        self.omari_talked = False
 
-    self.ZackId = 288899751850672138
-    self.yasmineID = 474609160487698463
-    self.omariID = 443940248993398786
-    self.sanitaID = 438175368483176448
-    self.xaviID = 186427201103593472
-    self.donalID = 357336111158263810
-    self.yasCount = 0
-    self.omariTalked = False
+    # Commands definitions
 
-  
-  # Commands definitions
-  @commands.command()
-  async def ye(self, ctx):
-    ret = post('https://api.kanye.rest')
-    await ctx.send(f"\"{ret.json()['quote']}\" - Kan(Ye) West")
+    @commands.command()
+    async def kanye_quote(self, ctx):
+        """Function that will display Kan(YE) quotes in "general" text channel"""
+        ret = post('https://api.kanye.rest')
+        await ctx.send(f"\"{ret.json()['quote']}\" - Kan(Ye) West")
 
-  # Text listener behavior
-  @commands.Cog.listener('on_message')
-  async def hi_detector(self, msg):
-    
-    # Tracking if Yasmine talked for the first time message
-    if msg.author.id == self.yasmineID and self.yasCount == 0:
-      await msg.reply('Please stop typing in this chat')
-      self.yasCount += 1
+    # Text listener behavior
+    @commands.Cog.listener('on_message')
+    async def message_reponse(self, msg):
+        """Command that will listen in general chat and give wanted responses"""
+        # Tracking if Yasmine talked for the first time message
+        if msg.author.id == YAS_ID and self.yas_count == 0:
+            await msg.reply('Please stop typing in this chat')
+            self.yas_count += 1
 
-    # After the first talked from Yasmine it will now track and give them 5 left talks until ban
-    elif msg.author.id == self.yasmineID and self.yasCount > 0 and self.yasCount < 6:
-      await msg.reply(f"You have { 5 - self.yasCount } messages left.")
-      await msg.add_reaction("👎")
-      self.yasCount += 1
-    # Checks if Omari talks and will reply with a nice message
-    elif msg.author.id == self.omariID and self.omariTalked != True:
-      await msg.reply("Welcome back King")
-      await msg.add_reaction("👑")
-      self.omariTalked = True
-    
-    # Will always react to Omari's and I messages
-    elif msg.author.id == self.omariID or msg.author.id == self.ZackId:
-      await msg.add_reaction("👑")
+        # After the first talked from Yasmine it will now track and give them 5 left talks until ban
+        elif msg.author.id == YAS_ID and self.yas_count > 0 and self.yas_count < 6:
+            await msg.reply(f"You have { 5 - self.yas_count } messages left.")
+            await msg.add_reaction("👎")
+            self.yas_count += 1
+        # Checks if Omari talks and will reply with a nice message
+        elif msg.author.id == OMARI_ID and self.omari_talked is not True:
+            await msg.reply("Welcome back King")
+            await msg.add_reaction("👑")
+            self.omari_talked = True
 
-    # Reaction for Sanita
-    elif msg.author.id == self.sanitaID:
-      await msg.add_reaction("🍓")
+        # Will always react to Omari's and I messages
+        elif msg.author.id == OMARI_ID or msg.author.id == ZACK_ID:
+            await msg.add_reaction("👑")
 
-    # Reaction for Xavi
-    elif msg.author.id == self.xaviID:
-      await msg.add_reaction("🦙")
-    
-    # Reaction for Donal
-    elif msg.author.id == self.donalID:
-      await msg.add_reaction("🍣")
-    
-  # @commands.command()
-  # async def deleteMsg(self, ctx, *, msg):
-  #   await 
+        # Reaction for Sanita
+        elif msg.author.id == SANITA_ID:
+            await msg.add_reaction("🍓")
 
+        # Reaction for Xavi
+        elif msg.author.id == XAVI_ID:
+            await msg.add_reaction("🦙")
 
-  # @commands.Cog.listener('on_message')
-  # async def spam(self, msg):
-  #   if msg.author.id == self.ZackId and 'no' in msg.content:
-  #     user = await self.bot.fetch_user(self.yasmineID)
-  #     for i in range(10):  
-  #       await user.send("Stop Typing")
-  #       await asyncio.sleep(1)
-    
+        # Reaction for Donal
+        elif msg.author.id == DONAL_ID:
+            await msg.add_reaction("🍣")
 
+    # @commands.command()
+    # async def deleteMsg(self, ctx, *, msg):
+    #   await
 
-
+    # @commands.Cog.listener('on_message')
+    # async def spam(self, msg):
+    #   if msg.author.id == self.ZackId and 'no' in msg.content:
+    #     user = await self.bot.fetch_user(self.yasmineID)
+    #     for i in range(10):
+    #       await user.send("Stop Typing")
+    #       await asyncio.sleep(1)
 
 
 def setup(bot):
-  bot.add_cog(functionality(bot))
+    """Setup for the bot"""
+    bot.add_cog(Functionality(bot))
