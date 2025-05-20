a77c745318997fbbf6553d2e013e20f22807698b
Zachary Santamaria <zsantamaria@protonmail.com>
2025-05-19 22:29:44 -0500

    RUFF formatt

--- bot.py
+++ bot.py
@@ -5,39 +5,43 @@ import discord
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = getenv('MAIN_TOKEN')
+TOKEN = getenv("MAIN_TOKEN")
 
 # Getting specific IDs from people in the server
-ZACK_ID = int(getenv('ZACK_ID'))
-OMARI_ID = int(getenv('OMARI_ID'))
-SANITA_ID = int(getenv('SANITA_ID'))
-XAVI_ID = int(getenv('XAVI_ID'))
-DONAL_ID = int(getenv('DONAL_ID'))
-RYAN_ID = int(getenv('RYAN_ID'))
-MEL_ID = int(getenv('MEL_ID'))
-CAIMAN_ID = int(getenv('CAIMAN_ID'))
+LANDO_ID = int(getenv("LANDO_ID"))
+ZACK_ID = int(getenv("ZACK_ID"))
+OMARI_ID = int(getenv("OMARI_ID"))
+SANITA_ID = int(getenv("SANITA_ID"))
+XAVI_ID = int(getenv("XAVI_ID"))
+DONAL_ID = int(getenv("DONAL_ID"))
+RYAN_ID = int(getenv("RYAN_ID"))
+MEL_ID = int(getenv("MEL_ID"))
+CAIMAN_ID = int(getenv("CAIMAN_ID"))
 
 # Getting channel IDs
-CHANNEL = int(getenv('CHANNEL_ID'))
+CHANNEL = int(getenv("CHANNEL_ID"))
 
 intents = discord.Intents.default()
-intents.members = True # pylint: disable=assigning-non-slot
+intents.members = True  # pylint: disable=assigning-non-slot
 client = discord.Client(intents=intents)
 
 
 @client.event
 async def on_ready():
     """Setting to the bots on ready"""
-    print(f'We have logged in as {client.user}')
+    print(f"We have logged in as {client.user}")
     game = discord.Game("programming with dad")
     await client.change_presence(status=discord.Status.online, activity=game)
+
 
 @client.event
 async def on_member_join(member):
     """A custom message for new members that join.
     The idea is to base it off of chappie from the movie."""
-    await member.send("What's up fuck mother. I am a creation from my dad Zack. \
-    I will eliminate you if you are mean. Have fun in the server!")
+    await member.send(
+        "What's up fuck mother. I am a creation from my dad Zack. \
+    I will eliminate you if you are mean. Have fun in the server!"
+    )
 
 
 @client.event
@@ -45,16 +49,17 @@ async def on_member_remove(member):
     """A cusom message will be made when someone is kicked or removed.
     This message will be somewhat based on the movie with my own twist."""
     general_channel = client.get_channel(CHANNEL)
-    await general_channel.send(f"Sorry dad but I wanted to tell you that {member.name} \
-        just left the server.")
-
+    await general_channel.send(
+        f"Sorry dad but I wanted to tell you that {member.name} \
+        just left the server."
+    )
 
 
 @client.event
 async def on_message(message):
     """This event is used for looking through a new message and will find the specified authors and
-     will do an action. The current version is only reacting to the messages. Could possibly add
-     a counter and track however many types a person types then bans them"""
+    will do an action. The current version is only reacting to the messages. Could possibly add
+    a counter and track however many types a person types then bans them"""
     if message.author == client.user:
         return
 
@@ -84,8 +89,11 @@ async def on_message(message):
     elif message.author.id == CAIMAN_ID:
         await message.add_reaction("🦂")
 
+    elif message.author.id == LANDO_ID:
+        await message.add_reaction("🤓")
+
     else:
-        print(f'{message.author} has typed {message.content}')
+        print(f"{message.author} has typed {message.content}")
 
 
 client.run(TOKEN)
